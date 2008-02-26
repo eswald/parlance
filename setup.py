4e764970fd1b03895873987b1d8620cb69e4a322
eswald <eswald@power-glide.com>
2008-02-26 13:49:43 -0700

    Adding relevant classifiers to the setup script.

--- setup.py
+++ setup.py
@@ -15,21 +15,10 @@ use_setuptools()
 from setuptools import setup
 
 setup(
+    # Provided items
     name = "Parlance",
     version = "1.4.0",
     packages = ["parlance"],
-    
-    # Project metadata
-    author = "Eric Wald",
-    author_email = "breadman@users.sourceforge.net",
-    description = "A framework for playing the Diplomacy board game over a network.",
-    license = "Artistic License 2.0",
-    keywords = "daide diplomacy board game server",
-    url = "https://sourceforge.net/projects/parlance/",
-    
-    # Installation options
-    zip_safe = True,
-    test_suite = "parlance.test",
     entry_points = {
         "console_scripts": [
             "parlance-server = parlance.server:run",
@@ -43,6 +32,29 @@ setup(
         ],
     },
     
+    # Project metadata
+    author = "Eric Wald",
+    author_email = "breadman@users.sourceforge.net",
+    description = "A framework for playing the Diplomacy board game over a network.",
+    license = "Artistic License 2.0",
+    keywords = "daide diplomacy board game server",
+    url = "https://sourceforge.net/projects/parlance/",
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Environment :: Console :: Curses",
+        "Environment :: No Input/Output (Daemon)",
+        "License :: OSI Approved :: Artistic License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Games/Entertainment :: Board Games",
+        "Topic :: Games/Entertainment :: Turn Based Strategy",
+    ],
+    
+    # Installation options
+    zip_safe = True,
+    test_suite = "parlance.test",
     package_data = {
         "parlance": ["data/*.html"],
     },
