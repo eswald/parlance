b460246af53299aa195fe32d3ff18cdb3dc7bedc
Eric Wald <pydip@arch.brainshell.org>
2008-07-24 21:04:57 -0600

    Distance calculations for TeddyBot

--- bots/parang/teddybot.py
+++ bots/parang/teddybot.py
@@ -6,8 +6,9 @@ r'''TeddyBot - A Diplomacy bot that attempts to choose targets.
     commercial purposes without permission from the authors is prohibited.
 '''#"""#'''
 
-from parlance.player import Player
+from parlance.functions import Infinity, defaultdict
 from parlance.orders import OrderSet
+from parlance.player import Player
 
 class TeddyBot(Player):
     r'''A bot that attempts to choose targets carefully.
@@ -17,6 +18,37 @@ class TeddyBot(Player):
         orders = OrderSet(self.power)
         orders.complete_set(self.map)
         self.submit_set(orders)
+    
+    def process_map(self):
+        r'''Performs supplemental processing on the map.
+            For TeddyBot, this involves distance and centrality calculations.
+            Returns whether to accept the MAP message.
+        '''#"""#'''
+        self.distance = distance = defaultdict(lambda: Infinity)
+        
+        coasts = self.map.coasts
+        for source in coasts:
+            for border in coasts[source].borders_out:
+                distance[(source, border)] = 1
+        for mid in coasts:
+            for source in coasts:
+                for sink in coasts:
+                    dist = distance[(source, mid)] + distance[(mid, sink)]
+                    if dist < distance[(source, sink)]:
+                        distance[(source, sink)] = dist
+        
+        spaces = self.map.spaces
+        for source in spaces:
+            for border in spaces[source].borders_out:
+                distance[(source, border)] = 1
+        for mid in spaces:
+            for source in spaces:
+                for sink in spaces:
+                    dist = distance[(source, mid)] + distance[(mid, sink)]
+                    if dist < distance[(source, sink)]:
+                        distance[(source, sink)] = dist
+        
+        return True
 
 
 def run():
--- bots/parang/testing.py
+++ bots/parang/testing.py
@@ -9,10 +9,11 @@ r'''Test cases for Parang bots
 import unittest
 
 from parlance.config       import variants
-from parlance.player       import HoldBot
+from parlance.functions    import Infinity
 from parlance.gameboard    import Variant
 from parlance.language     import protocol
 from parlance.tokens       import SUB
+from parlance.player       import HoldBot
 from parlance.test.player  import BotTestCase
 from parlance.test.network import NetworkTestCase
 
@@ -97,6 +98,43 @@ class TeddyBotTestCase(BotTestCase):
         now = NOW (FAL, 1901) (TUR, AMY, CON)
         expected = [[TUR, AMY, CON], MTO, BUL]
         self.assertOrder(now, None, TUR, expected)
+    
+    def test_fleet_distance(self):
+        from parlance.tokens import FLT
+        from parlance.xtended import POR, FIN
+        self.start_game()
+        dist = self.player.distance[((FLT, POR, None), (FLT, FIN, None))]
+        self.failUnlessEqual(dist, 6)
+    def test_fleet_distance_coastal(self):
+        from parlance.tokens import FLT, NCS
+        from parlance.xtended import SPA, PIE
+        self.start_game()
+        dist = self.player.distance[((FLT, SPA, NCS), (FLT, PIE, None))]
+        self.failUnlessEqual(dist, 4)
+    def test_fleet_distance_coastal_crawl(self):
+        # Fleets distance doesn't allow coast switching
+        from parlance.tokens import FLT
+        from parlance.xtended import MAR, GAS
+        self.start_game()
+        dist = self.player.distance[((FLT, MAR, None), (FLT, GAS, None))]
+        self.failUnlessEqual(dist, 3)
+    def test_army_distance(self):
+        from parlance.tokens import AMY
+        from parlance.xtended import POR, FIN
+        self.start_game()
+        dist = self.player.distance[((AMY, POR, None), (AMY, FIN, None))]
+        self.failUnlessEqual(dist, 8)
+    def test_army_distance_infinity(self):
+        from parlance.tokens import AMY
+        from parlance.xtended import TUN, NAP
+        self.start_game()
+        dist = self.player.distance[((AMY, TUN, None), (AMY, NAP, None))]
+        self.failUnlessEqual(dist, Infinity)
+    def test_convoy_distance(self):
+        from parlance.xtended import POR, FIN
+        self.start_game()
+        dist = self.player.distance[(POR, FIN)]
+        self.failUnlessEqual(dist, 5)
 
 class HuffTestCase(BotTestCase):
     bot_class = Project20M
