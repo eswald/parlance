9e5fd26f1d07a20367975ce1481ec2c26a45231a
Eric Wald <eswald@gmail.com>
2010-02-14 20:37:16 -0700

    Moving decision-makers into separate, composable classes.

--- extern/games.py
+++ extern/games.py
@@ -13,7 +13,6 @@ from parlance.util import s
 
 class TicTacToe(object):
     symbols = [" ", "X", "O"]
-    rewards = [400, 50, 900]
     square = [
         (0, 1),
         (2, 0),
@@ -26,26 +25,13 @@ class TicTacToe(object):
         (2, 1),
     ]
     
-    def __init__(self):
-        self.agent = Agent([])
-        self.collect_handlers()
+    def __init__(self, players):
+        self.players = [cls(self) for cls in players]
         self.err = None
         curses.wrapper(self.run)
         if self.err:
             e,x,c = self.err
             raise e,x,c
-    
-    def collect_handlers(self):
-        self.handlers = handlers = {}
-        for name in dir(self):
-            try:
-                f = getattr(self, name)
-                keys = f.im_func.input_keys
-            except AttributeError:
-                pass
-            else:
-                for key in keys:
-                    handlers[key] = f
     
     def run(self, win):
         try:
@@ -54,14 +40,11 @@ class TicTacToe(object):
             
             self.goto(1, 1)
             done = False
+            player = 1
             while not done:
-                self.goto(*self.pos)
                 win.refresh()
-                key = win.getkey()
-                #self.output("%r", key)
-                handler = self.handlers.get(key)
-                if handler:
-                    done = handler()
+                self.turn(player)
+                player ^= 3
             win.refresh()
         except:
             # Re-raise, but outside of the curses wrapper.
@@ -93,13 +76,27 @@ class TicTacToe(object):
         self.reset()
     
     def reset(self):
-        self.player = 1
         self.board = [0] * 9
         
         blank = self.symbols[0]
         for x in self.cols:
             for y in self.rows:
                 self.win.addch(y, x, blank)
+    
+    def turn(self, num):
+        player = self.players[num - 1]
+        self.output("Player %s", self.symbols[num])
+        while True:
+            self.goto(*self.pos)
+            self.win.refresh()
+            pos = player.generate()
+            if self.board[pos] == 0:
+                break
+            else:
+                self.output("Position %d already taken", pos)
+        self.fill(pos, num)
+        self.goto(*self.square[pos])
+        self.check()
     
     def output(self, line, *args):
         if args:
@@ -112,86 +109,24 @@ class TicTacToe(object):
         self.win.move(self.rows[row], self.cols[col])
         self.pos = (row, col)
     
-    def handler(*keys):
-        def decorator(f):
-            f.input_keys = keys
-            return f
-        return decorator
-    
-    def movement(f):
-        dy, dx = f()
-        @wraps(f)
-        def move(self):
-            y = (self.pos[0] + dy) % 3
-            x = (self.pos[1] + dx) % 3
-            self.goto(y, x)
-        return move
-    
-    @handler("KEY_UP", "k")
-    @movement
-    def move_up(): return -1, 0
-    
-    @handler("KEY_DOWN", "j")
-    @movement
-    def move_down(): return 1, 0
-    
-    @handler("KEY_LEFT", "h")
-    @movement
-    def move_left(): return 0, -1
-    
-    @handler("KEY_RIGHT", "l")
-    @movement
-    def move_right(): return 0, 1
-    
-    @handler("\n", " ", "x", "X", "o", "O")
-    def select(self):
-        player = self.player
-        pos = self.square.index(self.pos)
-        self.fill(pos, 1)
-        done = self.check()
-        if not done:
-            self.generate()
-            done = self.check()
-        return False
-    
-    def generate(self):
-        "Generate a move for the computer player."
-        msg = sum(x << (n*2) for n, x in enumerate(self.board))
-        for n in count(1):
-            msg = self.agent.process(msg)
-            pos = msg % 9
-            if not self.board[pos]:
-                self.output("%d round%s", n, s(n))
-                break
-        self.fill(pos, 2)
-        self.goto(*self.square[pos])
-    
-    def fill(self, pos, player):
-        if player != self.player:
-            # No cheating!
-            return False
-        
-        self.player ^= 3
-        self.board[pos] = player
-        r, c = self.square[pos]
-        y = self.rows[r]
-        x = self.cols[c]
-        self.win.addch(y, x, self.symbols[player])
-    
     def check(self):
         winner = self.winner()
         done = False
         if winner:
             self.output("Winner: %s", self.symbols[winner])
             done = True
+            for n, player in enumerate(self.players):
+                if winner == n + 1:
+                    points = 1
+                else:
+                    points = -1
+                player.result(points)
         elif all(self.board):
             self.output("Tie!")
             done = True
+            for player in self.players:
+                player.result(0)
         if done:
-            bonus = self.rewards[winner]
-            self.agent.reward(bonus)
-            self.win.refresh()
-            sleep(1)
             self.reset()
         return done
     
@@ -204,9 +139,112 @@ class TicTacToe(object):
                 if x+y+z == 12])
         return result
     
+    def fill(self, pos, player):
+        self.board[pos] = player
+        r, c = self.square[pos]
+        y = self.rows[r]
+        x = self.cols[c]
+        self.win.addch(y, x, self.symbols[player])
+    
+    def move(self, dy, dx):
+        y = (self.pos[0] + dy) % 3
+        x = (self.pos[1] + dx) % 3
+        self.goto(y, x)
+
+class HumanPlayer(object):
+    def __init__(self, game):
+        self.game = game
+        self.collect_handlers()
+    
+    def generate(self):
+        pos = None
+        while pos is None:
+            key = self.game.win.getkey()
+            #self.output("%r", key)
+            handler = self.handlers.get(key)
+            if handler:
+                pos = handler()
+        return pos
+    
+    def result(self, points):
+        self.game.win.refresh()
+        sleep(1)
+    
+    def collect_handlers(self):
+        self.handlers = handlers = {}
+        for name in dir(self):
+            try:
+                f = getattr(self, name)
+                keys = f.im_func.input_keys
+            except AttributeError:
+                pass
+            else:
+                for key in keys:
+                    handlers[key] = f
+    
+    def handler(*keys):
+        def decorator(f):
+            f.input_keys = keys
+            return f
+        return decorator
+    
+    @handler("KEY_UP", "k")
+    def move_up(self):
+        self.game.move(-1, 0)
+    
+    @handler("KEY_DOWN", "j")
+    def move_down(self):
+        self.game.move(1, 0)
+    
+    @handler("KEY_LEFT", "h")
+    def move_left(self):
+        self.game.move(0, -1)
+    
+    @handler("KEY_RIGHT", "l")
+    def move_right(self):
+        self.game.move(0, 1)
+    
+    @handler("\n", " ", "x", "X", "o", "O")
+    def select(self):
+        pos = self.game.square.index(self.game.pos)
+        return pos
+    
     @handler("\x1b", "q")
     def exit(self):
-        return True
+        sys.exit(0)
+
+class ComputerPlayer(object):
+    # Loss, Tie, Win
+    rewards = [50, 400, 900]
+    
+    def __init__(self, game):
+        self.agent = Agent([])
+        self.game = game
+    
+    def generate(self):
+        "Generate a move for the computer player."
+        msg = sum(x << (n*2) for n, x in enumerate(self.game.board))
+        for n in count(1):
+            msg = self.agent.process(msg)
+            pos = msg % 9
+            if not self.game.board[pos]:
+                self.game.output("%d round%s", n, s(n))
+                break
+        return pos
+    
+    def result(self, points):
+        bonus = self.rewards[points + 1]
+        self.agent.reward(bonus)
+
+class BasicPlayer(object):
+    def __init__(self, game):
+        self.game = game
+    
+    def generate(self):
+        return self.game.board.index(0)
+    
+    def result(self, points):
+        pass
 
 if __name__ == "__main__":
-    TicTacToe()
+    TicTacToe([HumanPlayer, HumanPlayer])
